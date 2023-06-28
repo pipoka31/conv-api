4d9ea8efbf9cd0dd54bdcd7bcf69532c2cde018d
GuiRissato <guilherme_andrade_rissato@hotmail.com>
2023-06-28 15:24:20 -0300

    auth ajustments

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,26 +1,17 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Hash from '@ioc:Adonis/Core/Hash'
+
 import User from 'App/Models/User'
 
 export default class AuthController {
-    public async login({ request, response, auth }: HttpContextContract) {
-       let {username, password} = request.all()
-    
-        try {
-    
-          const user = await User.findBy('user_name',username)
-          
-         
-          
-          if (user) {
-            const isPasswordValid = await user.verifyPassword(password)
-            console.log(isPasswordValid)
-            return isPasswordValid
-          } else {
-            return response.status(401).json({ error: 'Invalid credentials' });
-          }
-        } catch {
-          return response.unauthorized('Invalid credentials')
-        }
-      }
+  
+ public async  login({auth,request}){
+  const username = request.input('username')
+  const password = request.input('password')
+  console.log(username, password)
+  try {
+    await auth.attempt(username,password)
+  } catch (error) {
+    console.log('error auth',error)
+  }
+ }
 }
