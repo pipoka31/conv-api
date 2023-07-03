e0995aedbd699a615cb2e64b0c2bfa82a015d4c2
kass <lucasfiorirosa@gmail.com>
2023-07-03 10:55:59 -0300

    feat: add porject to git

--- app/database/conv-schema.sql
+++ app/database/conv-schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE messages (
     "id" SERIAL NOT NULL,
     "id_user" INTEGER NOT NULL,
     "text" TEXT NOT NULL,
-    "createdAt" DATE NOT NULL,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
     PRIMARY KEY ("id"),
     CONSTRAINT "message_id_user_foreign" FOREIGN KEY ("id_user") REFERENCES users ("id")
 );
@@ -36,25 +36,3 @@ CREATE TABLE conversations (
 );
 
 CREATE INDEX message_id_user_index ON messages ("id_user");
-
--- --insertions
-INSERT INTO "users" ("name", "userName", "password", "active", "createdAt", "updatedAt")
-SELECT 'John Doe', 'johndoe', 'password123', true, DATE '2023-06-12',  DATE '2023-06-12'
-UNION ALL
-SELECT 'Jane Smith', 'janesmith', 'password456', true, DATE '2023-06-12', DATE '2023-06-12'
-UNION ALL
-SELECT 'Robert Johnson', 'robertjohnson', 'password789', true, DATE '2023-06-12', DATE '2023-06-12';
-
-INSERT INTO "messages" ("id_user", "text", "createdAt")
-SELECT 1, 'Hello, how are you?', DATE '2023-06-12'
-UNION ALL
-SELECT 2, 'I am doing well. Thanks!', DATE '2023-06-12'
-UNION ALL
-SELECT 3, 'Nice to meet you!', DATE '2023-06-12';
-
-INSERT INTO "conversations" ("id_userSender", "id_userReciever", "id_message", "dateUpdate")
-SELECT 1, 2, 1, DATE '2023-06-12'
-UNION ALL
-SELECT 2, 1, 2, DATE '2023-06-12'
-UNION ALL
-SELECT 3, 1, 3, DATE '2023-06-12';
